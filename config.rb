da8f4067d14119bf09055b57ac270dff67c36d09
Brent Hardinge <designerbrent@gmail.com>
2013-04-10 08:48:42 -0700

    intial commit

---
+++ config.rb
@@ -0,0 +1,35 @@
+# Require any additional compass plugins here.
+
+require 'compass'
+
+
+# Set this to the root of your project when deployed:
+# http_path = "/sites/default/themes/bible/"
+# http_path = "/sites/default/themes/bible/"
+
+css_dir = "styles"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# You can select your preferred output style here (can be overridden via the command line):
+output_style = :expanded  # :expanded or :nested or :compact or :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+
+
+# To enable relative paths to assets via compass helper functions. Since Drupal
+# themes can be installed in multiple locations, we don't need to worry about
+# the absolute path to the theme from the server root.
+relative_assets = true
